70fdd04a52d6856cd13516ce7ce9cf89a3b1682f
Shahdamr2 <170415831+Shahdamr2@users.noreply.github.com>
2026-09-15 23:19:22 +0200

    Question 2

--- Assignment07/Program.cs
+++ Assignment07/Program.cs
@@ -14,6 +14,19 @@
             // suitable for complex objects and relationships between objects
 
             #endregion
+            #region Question 2
+
+            // a) Shipment is the parent class
+
+            // b) ExpressShipment is the child class
+
+            // c) ExpressShipment inherits the TrackingCode property from Shipment
+            // ExtraFee is a new property in ExpressShipment
+
+            // d) Inheritance allows us to reuse the same properties and methods
+            // without duplicating the code in multiple classes
+
+            #endregion
         }
     }
 }
