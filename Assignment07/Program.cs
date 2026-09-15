34deda95fde8f563363f957d15816e7bb6f65fc1
Shahdamr2 <170415831+Shahdamr2@users.noreply.github.com>
2026-09-15 23:16:59 +0200

    Question 1

--- Assignment07/Program.cs
+++ Assignment07/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1
+
+            // a) A class is a reference type while a struct is a value type
+            // When a struct is copied a separate copy is created
+            // When a class object is copied both variables refer to the same object
+
+            // b) Classes are more suitable for large applications because they are
+            // suitable for complex objects and relationships between objects
+
+            #endregion
         }
     }
 }
